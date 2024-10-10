8c02fcec07b2f121f0fae0a851ff29dc70851466
wandersoncorni <wandersoncorni@gmail.com>
2024-10-10 12:22:34 -0300

    Update

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -12,8 +12,15 @@ const iconMap = {
   pending: ClockIcon,
   invoices: InboxIcon,
 };
+import { fetchCardData } from '@/app/lib/data';
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
